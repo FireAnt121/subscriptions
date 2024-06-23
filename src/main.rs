7f8acfc0d33499f50b788b2e62db7efe54be0028
fireant <fireant@society>
2024-06-23 18:45:25 +0545

    fix(): fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
 use ztp::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to get configuration  file");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    ztp::startup::run(listener)
-        .expect("Failed to bind address")
-        .await
+    ztp::startup::run(listener, connection)?.await
 }
